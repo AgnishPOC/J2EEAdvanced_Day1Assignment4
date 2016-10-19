2b220a4d4d2f4744c5a2d2ec27a80002aff0ae8c
AgnishPOC <agnishshyam@outlook.com>
2016-10-19 12:06:14 +0530

    Add files via upload

---
+++ Assignment4/solvingExpressions.java
@@ -0,0 +1,15 @@
+package Day1.Assignment4;
+
+public class solvingExpressions {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		System.out.println("x + y * 2 = " + (x + y * 2));
+		System.out.println("x - y + 2 = " + (x - y + 2));
+		System.out.println("(x+y) * 2 = " + (x + y) * 2);
+		System.out.println("y % x = " + y % x);
+
+	}
+
+}
